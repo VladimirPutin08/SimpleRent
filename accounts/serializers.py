c0178824eaa38b1a73c6e5b01aa32f523e3bf28f
VladimirPutin08 <vladimirputin203013@gmail.com>
2025-12-08 16:56:09 +0530

    Update serializers.py

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -15,3 +15,22 @@ class RegisterSerializer(serializers.ModelSerializer):
             role=validated_data["role"],
         )
         return user
+
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ("id","username","email","password","role")
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
