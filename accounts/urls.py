ee6e8171512de8c16e60fc2cf657093f2f0ec061
VladimirPutin08 <vladimirputin203013@gmail.com>
2025-12-08 18:10:48 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,3 +4,11 @@ from .views import RegisterUserView
 urlpatterns = [
     path("register/", RegisterUserView.as_view(), name="register"),
 ]
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/accounts/', include('accounts.urls')),   # <-- Missing earlier
+    path('api/', include('accounts.jwt_urls')),        # or wherever your token urls are
+]
