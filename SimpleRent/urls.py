6577bb150184629f05e93e7d372ddc2b40acda4f
VladimirPutin08 <vladimirputin203013@gmail.com>
2025-12-08 16:35:55 +0530

    Update urls.py

--- SimpleRent/urls.py
+++ SimpleRent/urls.py
@@ -10,3 +10,15 @@ from django.urls import path, include
 urlpatterns = [
     path("api/accounts/", include("accounts.urls")),
 ]
+from django.contrib import admin
+from django.urls import path, include
+from django.http import JsonResponse
+
+def index(request):
+    return JsonResponse({"status": "ok", "message": "SimpleRent API is online 🚀"})
+
+urlpatterns = [
+    path("", index),                        # 👈 returns JSON
+    path("api/accounts/", include("accounts.urls")),
+    path("admin/", admin.site.urls),
+]
